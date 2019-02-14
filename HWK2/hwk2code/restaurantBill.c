485b71a2253a49a34198bfe0c8e23bbc363d189f
Alexa Tang <alexatang@cpp.edu>
2019-02-14 11:58:15 -0800

    third commit

--- HWK2/hwk2code/restaurantBill.c
+++ HWK2/hwk2code/restaurantBill.c
@@ -8,6 +8,7 @@
 
 int main()
 {
+	//Restaurant Bill
 	float mealCost = 88.67;
 	float taxPercent = 0.0675;
 	float tipPercent = 0.2;
@@ -19,6 +20,21 @@ int main()
 	float mWTax = mealCost + (mealCost*taxPercent);
 	float totalBill = mWTax + (mWTax * tipPercent);
 	printf("Total bill is $%.2f", totalBill);
+
+	//Ocean Levels
+	float rate = 1.5;
+	float five = rate*5.0;
+	float seven = rate * 7.0;
+	float ten = rate * 10.0;
+
+	printf("Number of millimeters higher that the current ocean's level will be in 5 years: %.1f\n", five);
+	printf("Number of millimeters higher that the current ocean's level will be in 5 years: %.1f\n", seven);
+	printf("Number of millimeters higher that the current ocean's level will be in 5 years: %.1f\n", ten);
+
+	//
+
+		return 0;
+
 	return 0;
 
 }
